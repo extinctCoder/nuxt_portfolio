6e5130228131aea8bb60553c144c45f4357adc0b
fiery.snowflake <extinctCoder@outlook.com>
2024-01-30 20:17:44 +0000

    Jan 31, 2024, 2:17 AM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   devtools: {
     enabled: true,
-
     timeline: {
-      enabled: true
-    }
-  }
-})
+      enabled: true,
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
