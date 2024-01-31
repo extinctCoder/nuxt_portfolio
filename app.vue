b90ca52515f4c7cf11216318f492350680b37ff2
darthVader <extinctCoder@outlook.com>
2024-01-31 11:26:37 +0000

    Jan 31, 2024, 5:26 PM

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
-  <div data-theme="light" class="h-screen flex flx-col">
+  <div data-theme="light" class="h-screen flex">
     <SideNav />
-    <div class="min-h-full w-full bg-green-400">
-      <div class="h-24">hi</div>
+    <div class="min-h-full w-full bg-green-400 flex">
+      <!-- <div class="">hi</div>
+      <div class="">bye</div> -->
+      <!-- <div class="h-24">hi</div>
       <div class="bg-black h-full">
         <div class="container mx-auto bg-white h-96"></div>
-      </div>
+      </div> -->
       <!-- <div class="h-full bg-red-700 overflow-auto"> -->
       <!-- <TestBed /> -->
       <!-- </div> -->
