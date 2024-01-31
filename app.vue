1b07219067beffa979d513129243c219715cd269
darthVader <extinctCoder@outlook.com>
2024-01-31 11:10:07 +0000

    Jan 31, 2024, 5:10 PM

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div data-theme="light" class="h-screen flex flx-col">
     <SideNav />
     <div class="flex flex-row">
-      <div></div>
+      <div class="bg-red-700">hi</div>
       <div class="min-h-full w-full overflow-auto bg-green-400">
         <div class="container mx-auto"><TestBed /></div>
       </div>
