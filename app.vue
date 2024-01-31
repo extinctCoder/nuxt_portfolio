8f784c6ff20dbc557372d111ec2bd7d87d544144
darthVader <extinctCoder@outlook.com>
2024-01-31 11:24:50 +0000

    Jan 31, 2024, 5:24 PM

--- app.vue
+++ app.vue
@@ -3,8 +3,8 @@
     <SideNav />
     <div class="min-h-full w-full bg-green-400">
       <div class="h-24">hi</div>
-      <div class="bg-black h-96">
-        <div class="container mx-auto bg-white h-48"></div>
+      <div class="bg-black h-full">
+        <div class="container mx-auto bg-white h-96"></div>
       </div>
       <!-- <div class="h-full bg-red-700 overflow-auto"> -->
       <!-- <TestBed /> -->
