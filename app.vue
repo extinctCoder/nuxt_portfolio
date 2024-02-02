9c8f078a02538114680fdd7ec7934a88b7d3a2f1
fiery.snowflake <extinctCoder@outlook.com>
2024-02-02 17:22:11 +0000

    Feb 2, 2024, 11:22 PM

--- app.vue
+++ app.vue
@@ -1,17 +1,3 @@
 <template>
-  <div data-theme="light" class="flex h-screen">
-    <!-- <SideNav /> -->
-    <div class="bg-red-400">bye</div>
-    <div class="flex min-h-full w-full bg-green-400">
-      <!-- <div class="">hi</div>
-      <div class="">bye</div> -->
-      <!-- <div class="h-24">hi</div>
-      <div class="bg-black h-full">
-        <div class="container mx-auto bg-white h-96"></div>
-      </div> -->
-      <!-- <div class="h-full bg-red-700 overflow-auto"> -->
-      <!-- <TestBed /> -->
-      <!-- </div> -->
-    </div>
-  </div>
+  <div>this is main code</div>
 </template>
