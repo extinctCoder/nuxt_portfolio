9667d282ba11c006a44c94bf7b747e5df51e6230
darthVader <extinctCoder@outlook.com>
2024-02-04 06:51:46 +0000

    Feb 4, 2024, 12:51 PM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="flex min-h-full w-96 min-w-64 flex-col gap-2 bg-white p-4">
-    <div class="grow bg-red-400"></div>
+    <div class="grow bg-red-400">
+      <div class="avatar">
+        <div class="w-24 rounded-xl">
+          <img
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          />
+        </div>
+      </div>
+    </div>
     <SidebarFooter />
   </div>
 </template>
