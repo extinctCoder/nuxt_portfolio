cbe62075514abda9a6c3b4ea265e46798a21d8f7
darthVader <extinctCoder@outlook.com>
2024-02-04 05:53:38 +0000

    Feb 4, 2024, 11:53 AM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -2,7 +2,11 @@
   <div class="flex min-h-full w-96 min-w-64 flex-col gap-2 bg-white p-4">
     <div class="grow bg-red-400"></div>
     <div class="flex h-24 items-center justify-center bg-green-500">
-      <Icon name="nuxt" />
+      <ul class="menu menu-horizontal rounded-box bg-base-200">
+        <li>
+          <a> </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
