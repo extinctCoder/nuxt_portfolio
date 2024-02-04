4d87dea24032de3a4cab759b5766a98a1b74ecf7
darthVader <extinctCoder@outlook.com>
2024-02-04 06:06:00 +0000

    Feb 4, 2024, 12:06 PM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="flex min-h-full w-96 min-w-64 flex-col gap-2 bg-white p-4">
     <div class="grow bg-red-400"></div>
-    <div class="flex h-24 items-center justify-center bg-green-500">
-      <Icon name="jam:fiverr" />
-      <Icon name="jam:twitter" />
-      <Icon name="jam:linkedin" />
-      <Icon name="jam:mail" />
-
-      <!-- <ul class="menu menu-horizontal rounded-box bg-base-200">
+    <div class="flex items-center justify-center">
+      <ul class="menu menu-horizontal">
         <li>
           <a>
-            <Icon name="jam:fiverr" class="text-white" />
+            <Icon name="jam:fiverr" />
           </a>
         </li>
         <li>
           <a>
-            <Icon name="jam:fiverr" class="text-white" />
+            <Icon name="jam:twitter" />
           </a>
         </li>
-      </ul> -->
+        <li>
+          <a>
+            <Icon name="jam:linkedin" />
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
