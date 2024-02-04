a8a29f0a103e23c7723a99cd20dbd90515108bd6
darthVader <extinctCoder@outlook.com>
2024-02-04 05:25:32 +0000

    Feb 4, 2024, 11:25 AM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
-    class="flex min-h-full w-96 min-w-64 flex-col justify-between bg-white p-4"
-  >
-    <div class="bg-red-400">3</div>
-    <div class="bg-green-500">3</div>
+  <div class="flex min-h-full w-96 min-w-64 flex-col bg-white p-4">
+    <div class="grow bg-red-400"></div>
+    <div class="inline-flex h-24 items-center justify-center bg-green-500">
+      bellow buttons
+    </div>
   </div>
 </template>
 
