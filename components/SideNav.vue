54c59faf515975b9ef213f722de5a53314d4848e
darthVader <extinctCoder@outlook.com>
2024-02-04 06:48:56 +0000

    Feb 4, 2024, 12:48 PM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="flex min-h-full w-96 min-w-64 flex-col gap-2 bg-white p-4">
     <div class="grow bg-red-400"></div>
-    <div class="flex items-center justify-center">
-      <ul class="menu menu-horizontal">
-        <li>
-          <a>
-            <Icon name="jam:fiverr" />
-          </a>
-        </li>
-        <li>
-          <a>
-            <Icon name="jam:twitter" />
-          </a>
-        </li>
-        <li>
-          <a>
-            <Icon name="jam:linkedin" />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <SidebarFooter />
   </div>
 </template>
 
---
+++ components/sidebar/SidebarFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex items-center justify-center">
+    <ul class="menu menu-horizontal">
+      <li>
+        <a>
+          <Icon name="jam:fiverr" />
+        </a>
+      </li>
+      <li>
+        <a>
+          <Icon name="jam:twitter" />
+        </a>
+      </li>
+      <li>
+        <a>
+          <Icon name="jam:linkedin" />
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
