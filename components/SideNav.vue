b4031882163bcea5fb3b999ec569f92bc887a68d
darthVader <extinctCoder@outlook.com>
2024-02-04 05:19:48 +0000

    Feb 4, 2024, 11:19 AM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="h-full w-96 min-w-64 bg-white shadow"></div>
+  <div class="flex min-h-full w-96 min-w-64 flex-row bg-white p-4">
+    <div>3</div>
+    <div>3</div>
+    <div>3</div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
