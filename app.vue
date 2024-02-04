d912d0d936d755cf5250f300e4cbeb43cb0130f6
darthVader <extinctCoder@outlook.com>
2024-02-04 05:43:15 +0000

    Feb 4, 2024, 11:43 AM

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div data-theme="light" class="h-screen bg-gray-400">
+  <!-- <div data-theme="light" class="h-screen bg-gray-400"> -->
+  <div class="h-screen">
     <SideNav />
   </div>
 </template>
