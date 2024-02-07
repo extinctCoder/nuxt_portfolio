889baa5d506371f288c6b210ac4f61f9bd05c7b2
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:44:51 +0000

    Feb 8, 2024, 3:44 AM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex min-h-full flex-col justify-between gap-2 bg-white p-4">
+  <div
+    class="flex min-h-full min-w-fit flex-col justify-between gap-2 bg-white p-4"
+  >
     <div class="flex flex-col">
       <SidebarIcon />
       <SidebarMenu />
