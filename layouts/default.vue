bfbed746706bc26b566eddd15580142dabdf1c68
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 20:50:25 +0000

    Feb 8, 2024, 2:50 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,11 +26,12 @@
         aria-label="close sidebar"
         className="drawer-overlay"
       ></label>
-      <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+      <SideNav />
+      <!-- <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
         {/* Sidebar content here */}
         <li><a>Sidebar Item 1</a></li>
         <li><a>Sidebar Item 2</a></li>
-      </ul>
+      </ul> -->
     </div>
   </div>
 </template>
