1841610ca14a6dac6c2bff6b62ec48df17db0625
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:38:45 +0000

    Feb 8, 2024, 3:38 AM

--- components/Header.vue
+++ components/Header.vue
@@ -4,6 +4,22 @@
       <label htmlFor="my-drawer-2" className="btn btn-square ">
         <Icon name="heroicons:bars-3-solid" class="inline-block h-5 w-5" />
       </label>
+      <button className="btn btn-square">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
       <!-- <button class="btn"></button> -->
     </div>
     <div class="flex-1">
@@ -11,19 +27,7 @@
     </div>
     <div class="flex-none">
       <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block h-5 w-5 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-          ></path>
-        </svg>
+        <Icon name="heroicons:bars-3-solid" class="inline-block h-5 w-5" />
       </button>
     </div>
   </div>
