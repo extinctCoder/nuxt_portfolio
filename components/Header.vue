6c06c09093c2a489a37d6838c5d4b7fee06be005
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:59:37 +0000

    Feb 8, 2024, 3:59 AM

--- components/Header.vue
+++ components/Header.vue
@@ -5,23 +5,6 @@
         <Icon name="heroicons:bars-3-solid" class="inline-block h-5 w-5" />
       </label>
     </div>
-    <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block h-5 w-5 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
-    </div>
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">daisyUI</a>
     </div>
