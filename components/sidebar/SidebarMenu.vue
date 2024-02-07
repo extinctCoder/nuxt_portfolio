3c891976e26d65be17c5d2014b94bbfef27fcf01
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 19:45:17 +0000

    Feb 8, 2024, 1:45 AM

--- components/sidebar/SidebarMenu.vue
+++ components/sidebar/SidebarMenu.vue
@@ -2,26 +2,13 @@
   <ul className="menu bg-base-200 ">
     <li>
       <a>
-        <Icon name="jam:linkedin" />
+        <Icon name="heroicons:home-solid" />
         Item 2
       </a>
     </li>
     <li>
       <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <Icon name="heroicons:home-20-solid" />
         Item 1
       </a>
     </li>
