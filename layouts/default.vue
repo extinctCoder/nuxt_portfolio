be001d80c75a9acaf83d0d550b87822ae3b9b0d9
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 22:45:12 +0000

    Feb 8, 2024, 4:45 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,9 @@
-<!-- <template>
-  <div data-theme="light" class="flex h-screen overflow-hidden">
-    <SideNav />
-    <div class="flex w-full flex-col">
-      <Header />
-      <div class="w-full overflow-y-auto scroll-smooth bg-gray-400">
-        <div class="container mx-auto"><slot /></div>
-      </div>
-    </div>
-  </div>
-</template> -->
 <template>
-  <!-- <div className="drawer lg:drawer-open">
-    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content flex flex-col items-center over">
-      <Header />
-
-      <div class="w-full overflow-y-auto scroll-smooth bg-gray-400">
-        <div class="container mx-auto"><slot /></div>
-      </div>
-    </div>
-    <div className="drawer-side">
-      <label
-        htmlFor="my-drawer-2"
-        aria-label="close sidebar"
-        className="drawer-overlay"
-      ></label>
-      <SideNav />
-    </div>
-  </div> -->
   <div class="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
       <Header />
-      <!-- <div class="w-full overflow-y-auto scroll-smooth bg-gray-400"> -->
-      <div class="container mx-auto mt-4 bg-gray-400"><slot /></div>
-      <!-- </div> -->
+      <div class="container mx-auto mt-4 bg-gray-400 px-4"><slot /></div>
     </div>
     <div class="drawer-side">
       <label
