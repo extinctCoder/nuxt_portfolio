32eff167b2ed06bb3b2d0112ca46ca9115c07fd0
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:31:29 +0000

    Feb 8, 2024, 3:31 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,11 +14,7 @@
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content flex flex-col items-center over">
       <Header />
-      <label
-        htmlFor="my-drawer-2"
-        className="btn btn-primary drawer-button lg:hidden"
-        >Open drawer</label
-      >
+
       <div class="w-full overflow-y-auto scroll-smooth bg-gray-400">
         <div class="container mx-auto"><slot /></div>
       </div>
