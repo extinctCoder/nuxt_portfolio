acb003131df04ab9a77aa1edf706013bb2438641
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 20:10:31 +0000

    Feb 8, 2024, 2:10 AM

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -2,11 +2,10 @@
   <div
     class="flex min-h-full w-96 min-w-64 flex-col justify-between gap-2 bg-white p-4"
   >
-    <div class="flex flex-col items-center justify-center gap-8 bg-red-400 p-4">
+    <div class="flex flex-col gap-8 p-4">
       <SidebarIcon />
+      <SidebarMenu />
     </div>
-    <SidebarMenu />
-    <SidebarCard />
     <SidebarCard />
     <SidebarFooter />
   </div>
