3304bb2ea599c52b38899b60cf4027e7a48b36c9
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 20:15:08 +0000

    Feb 8, 2024, 2:15 AM

--- components/sidebar/SidebarFooter.vue
+++ components/sidebar/SidebarFooter.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="flex items-center justify-center">
-    <ul class="menu menu-horizontal menu-lg">
-      <li>
-        <a>
-          <Icon name="jam:fiverr" />
-        </a>
-      </li>
-      <li>
-        <a>
-          <Icon name="jam:twitter" />
-        </a>
-      </li>
-      <li>
-        <a>
-          <Icon name="jam:linkedin" />
-        </a>
-      </li>
-    </ul>
-  </div>
+  <ul class="menu menu-horizontal menu-lg items-center justify-around">
+    <li>
+      <a>
+        <Icon name="jam:fiverr" />
+      </a>
+    </li>
+    <li>
+      <a>
+        <Icon name="jam:fiverr" />
+      </a>
+    </li>
+    <li>
+      <a>
+        <Icon name="jam:fiverr" />
+      </a>
+    </li>
+    <li>
+      <a>
+        <Icon name="jam:twitter" />
+      </a>
+    </li>
+    <li>
+      <a>
+        <Icon name="jam:linkedin" />
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts" setup></script>
