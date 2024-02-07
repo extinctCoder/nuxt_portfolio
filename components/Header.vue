0e0ac9aad77849150c8a96fed267bf24fcef54f9
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:43:55 +0000

    Feb 8, 2024, 3:43 AM

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar bg-base-100">
-    <div class="flex-none">
+    <div class="flex-none lg:hidden">
       <label htmlFor="my-drawer-2" class="btn btn-square btn-ghost">
         <Icon name="heroicons:bars-3-solid" class="inline-block h-5 w-5" />
       </label>
@@ -8,11 +8,7 @@
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">daisyUI</a>
     </div>
-    <div class="flex-none">
-      <label class="btn btn-square btn-ghost">
-        <Icon name="heroicons:bars-3-solid" class="inline-block h-5 w-5" />
-      </label>
-    </div>
+    <div class="flex-none">work in profress</div>
   </div>
 </template>
 
