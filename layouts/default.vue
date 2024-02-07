1be5b64dfeee6a873e182464d120d1af9c4ea53a
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 20:53:09 +0000

    Feb 8, 2024, 2:53 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,15 +10,15 @@
   </div>
 </template> -->
 <template>
-  <div className="drawer lg:drawer-open">
+  <div className="drawer md:drawer-open">
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content flex flex-col items-center justify-center">
-      {/* Page content here */}
       <label
         htmlFor="my-drawer-2"
         className="btn btn-primary drawer-button lg:hidden"
         >Open drawer</label
       >
+      {/* Page content here */}
     </div>
     <div className="drawer-side">
       <label
