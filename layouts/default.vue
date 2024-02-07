2b33bb6ec83073c4f98943167b2bd45fb383bf64
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:18:17 +0000

    Feb 8, 2024, 3:18 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,7 @@
 <template>
   <div className="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content flex flex-col items-center">
+    <div className="drawer-content flex flex-col items-center overflow-hidden">
       <Header />
       <label
         htmlFor="my-drawer-2"
