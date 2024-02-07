ffdfc0ff993cbf9c41b0f095a179af939ceda1a7
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:02:39 +0000

    Feb 8, 2024, 3:02 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,32 +9,7 @@
     </div>
   </div>
 </template> -->
-<template>
-  <div className="drawer md:drawer-open">
-    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content flex flex-col items-center justify-center">
-      <label
-        htmlFor="my-drawer-2"
-        className="btn btn-primary drawer-button lg:hidden"
-        >Open drawer</label
-      >
-      {/* Page content here */}
-    </div>
-    <div className="drawer-side">
-      <label
-        htmlFor="my-drawer-2"
-        aria-label="close sidebar"
-        className="drawer-overlay"
-      ></label>
-      <SideNav />
-      <!-- <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-        {/* Sidebar content here */}
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-      </ul> -->
-    </div>
-  </div>
-</template>
+<template></template>
 
 <script lang="ts" setup></script>
 
