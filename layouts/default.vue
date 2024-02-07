03350dfb642fecc3d167cffce8c9b19102ae2319
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 20:34:12 +0000

    Feb 8, 2024, 2:34 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div data-theme="light" class="flex h-screen overflow-hidden bg-gray-400">
     <SideNav />
-    <!-- <div class="flex flex-col"> -->
-    <!-- <Header /> -->
-    <div class="w-full overflow-y-auto scroll-smooth">
-      <div class="container mx-auto"><slot /></div>
+    <div class="flex flex-col">
+      <Header />
+      <div class="w-full overflow-y-auto scroll-smooth">
+        <div class="container mx-auto"><slot /></div>
+      </div>
     </div>
-    <!-- </div> -->
   </div>
 </template>
 
