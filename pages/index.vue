71f14cb2e7cc148320c2ac27cbb056b309c35a3a
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 22:58:51 +0000

    Feb 8, 2024, 4:58 AM

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   hello
   <TestBed />
+  <div class="flex flex-col gap-4">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
 </template>
 
 <script lang="ts" setup></script>
