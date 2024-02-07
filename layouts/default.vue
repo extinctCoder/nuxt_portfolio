bdeff5570518d1928b592a6260663858c6d2f98f
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 20:45:47 +0000

    Feb 8, 2024, 2:45 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div data-theme="light" class="flex h-screen overflow-hidden">
     <SideNav />
     <div class="flex w-full flex-col">
@@ -6,6 +6,31 @@
       <div class="w-full overflow-y-auto scroll-smooth bg-gray-400">
         <div class="container mx-auto"><slot /></div>
       </div>
+    </div>
+  </div>
+</template> -->
+<template>
+  <div className="drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+    <!-- <div className="drawer-content flex flex-col items-center justify-center">
+      {/* Page content here */}
+      <label
+        htmlFor="my-drawer-2"
+        className="btn btn-primary drawer-button lg:hidden"
+        >Open drawer</label
+      >
+    </div> -->
+    <div className="drawer-side">
+      <label
+        htmlFor="my-drawer-2"
+        aria-label="close sidebar"
+        className="drawer-overlay"
+      ></label>
+      <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        {/* Sidebar content here */}
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
     </div>
   </div>
 </template>
