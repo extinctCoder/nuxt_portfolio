0826c64eefbf87261c71768abd8a82215f31004e
fiery.snowflake <extinctCoder@outlook.com>
2024-02-07 21:20:14 +0000

    Feb 8, 2024, 3:20 AM

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
       <button className="btn btn-square btn-ghost">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="black"
+          fill="none"
           viewBox="0 0 24 24"
           className="inline-block w-5 h-5 stroke-current"
         >
