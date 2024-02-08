c6f3c232e2939b8a6902d047f97bbc845ea53943
darthVader <extinctCoder@outlook.com>
2024-02-08 10:24:04 +0000

    Feb 8, 2024, 4:24 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex cursor-pointer gap-2 rounded border">
-    <div class="flex h-48 flex-col justify-between gap-2 bg-black p-4">
+  <div class="flex h-56 cursor-pointer gap-2 rounded border">
+    <div class="flex flex-col justify-between gap-2 bg-black p-4">
       <div class="flex flex-col gap-4">
         <h2 class="card-title">New album is released!</h2>
         <TestBed />
