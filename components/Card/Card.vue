fd44cfcabf4987902f8d60ccc35f490a5ad680ad
darthVader <extinctCoder@outlook.com>
2024-02-08 10:06:42 +0000

    Feb 8, 2024, 4:06 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -26,6 +26,22 @@
       />
     </figure>
   </div>
+
+  <div class="card bg-base-100 shadow-xl lg:card-side">
+    <figure>
+      <img
+        src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+        alt="Album"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">New album is released!</h2>
+      <p>Click the button to listen on Spotiwhy app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Listen</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
