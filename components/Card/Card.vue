62861d2c7653eb6e0d5c8a81ca9667a090af96a5
darthVader <extinctCoder@outlook.com>
2024-02-08 07:19:03 +0000

    Feb 8, 2024, 1:19 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -17,14 +17,27 @@
     </figure>
   </div> -->
 
-  <div class="border-ro flex gap-2 rounded border">
+  <div class="border-ro flex cursor-pointer gap-2 rounded border">
     <div class="flex flex-col justify-between p-4">
       <div class="flex flex-col gap-4">
         <h2 class="card-title">New album is released!</h2>
         <TestBed />
       </div>
+      <div class="flex justify-between">
+        <div class="flex gap-4">
+          <div class="inline-flex items-center gap-2">
+            <Icon name="heroicons:calendar-solid" />
+            <span>31/12/2006</span>
+          </div>
+          <div class="inline-flex items-center gap-2">
+            <Icon name="heroicons:folder-open-solid" />
+            <span>Blogging, Demo</span>
+          </div>
+        </div>
+        <Icon name="heroicons:hand-thumb-up-solid" />
+      </div>
     </div>
-    <figure class="w-1/3">
+    <figure class="w-1/3 min-w-80">
       <img
         src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
         alt="Album"
@@ -34,7 +47,7 @@
 
   <!-- <div class="cursor-pointer overflow-hidden rounded border p-4"> -->
   <!-- <div class="flex grow gap-2"> -->
-  <div class="flex justify-between">
+  <!-- <div class="flex justify-between">
     <div class="flex gap-4">
       <div class="inline-flex items-center gap-2">
         <Icon name="heroicons:calendar-solid" />
@@ -46,7 +59,7 @@
       </div>
     </div>
     <Icon name="heroicons:hand-thumb-up-solid" />
-  </div>
+  </div> -->
   <!-- </div> -->
   <!-- </div> -->
 </template>
