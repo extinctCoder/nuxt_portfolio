b9323c505370b7cda96f95f5623e3029371cad72
darthVader <extinctCoder@outlook.com>
2024-02-08 10:15:20 +0000

    Feb 8, 2024, 4:15 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -19,12 +19,13 @@
         <Icon name="heroicons:hand-thumb-up-solid" />
       </div>
     </div>
-    <figure class="w-1/3 min-w-80 max-w-96">
-      <NuxtImg
-        src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-        alt="Album"
-      />
-    </figure>
+    <!-- <figure class="w-1/3 min-w-80 max-w-96 "> -->
+    <NuxtImg
+      src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+      alt="Album"
+      class="h-32"
+    />
+    <!-- </figure> -->
   </div>
 
   <!-- <div class="card border bg-base-100 lg:card-side">
