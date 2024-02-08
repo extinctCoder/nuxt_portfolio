f206a5c6ac195b9f0850103386f4022aa06f101c
darthVader <extinctCoder@outlook.com>
2024-02-08 10:29:41 +0000

    Feb 8, 2024, 4:29 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -43,6 +43,35 @@
       />
     </figure>
   </div> -->
+
+  <div class="flex h-screen items-center justify-center">
+    <div class="contain max-h-full">
+      <div class="flex flex-col gap-4">
+        <h2 class="card-title">New album is released!</h2>
+        <TestBed />
+      </div>
+      <div class="flex justify-between">
+        <div class="flex gap-4">
+          <div class="inline-flex items-center gap-2">
+            <Icon name="heroicons:calendar-solid" />
+            <span>31/12/2006</span>
+          </div>
+          <div class="inline-flex items-center gap-2">
+            <Icon name="heroicons:folder-open-solid" />
+            <span>Blogging, Demo</span>
+          </div>
+        </div>
+        <Icon name="heroicons:hand-thumb-up-solid" />
+      </div>
+    </div>
+    <!-- <figure class="w-1/3 min-w-80 max-w-96 "> -->
+    <NuxtImg
+      src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
+      alt="Album"
+      class="h-full"
+    />
+    <!-- </figure> -->
+  </div>
 </template>
 
 <script lang="ts" setup></script>
