d90d3d2795aa18efec7e5a429abf9c5398822fc7
darthVader <extinctCoder@outlook.com>
2024-02-08 10:03:21 +0000

    Feb 8, 2024, 4:03 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -1,22 +1,4 @@
 <template>
-  <!-- <div class="card border bg-base-100 lg:card-side">
-    <div class="card-body">
-      <h2 class="card-title">New album is released!</h2>
-      <p>
-        <TestBed />
-      </p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Listen</button>
-      </div>
-    </div>
-    <figure class="w-1/3">
-      <img
-        src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-        alt="Album"
-      />
-    </figure>
-  </div> -->
-
   <div class="border-ro flex cursor-pointer gap-2 rounded border">
     <div class="flex flex-col justify-between gap-2 p-4">
       <div class="flex flex-col gap-4">
@@ -44,24 +26,6 @@
       />
     </figure> -->
   </div>
-
-  <!-- <div class="cursor-pointer overflow-hidden rounded border p-4"> -->
-  <!-- <div class="flex grow gap-2"> -->
-  <!-- <div class="flex justify-between">
-    <div class="flex gap-4">
-      <div class="inline-flex items-center gap-2">
-        <Icon name="heroicons:calendar-solid" />
-        <span>31/12/2006</span>
-      </div>
-      <div class="inline-flex items-center gap-2">
-        <Icon name="heroicons:folder-open-solid" />
-        <span>Blogging, Demo</span>
-      </div>
-    </div>
-    <Icon name="heroicons:hand-thumb-up-solid" />
-  </div> -->
-  <!-- </div> -->
-  <!-- </div> -->
 </template>
 
 <script lang="ts" setup></script>
