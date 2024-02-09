5c206a29a5236b9c991cf89c21f9fc18cb0774d6
fiery.snowflake <extinctCoder@outlook.com>
2024-02-09 17:49:21 +0000

    Feb 9, 2024, 11:49 PM

--- layouts/post-list.vue
+++ layouts/post-list.vue
@@ -3,21 +3,19 @@
     <div class="flex grow flex-col gap-4 bg-gray-400">
       <slot />
     </div>
-    <div class="min-w-64 max-w-72 flex-none bg-stone-400">
-      <br />
-      <br />
-      <br />
-      <ul class="menu rounded-box bg-base-200">
-        <li>
-          <h2 class="menu-title">Title</h2>
-          <ul>
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </li>
-      </ul>
-      -->
+    <div class="w-72 flex-none bg-stone-400">
+      <div class="sticky top-0">
+        <ul class="menu bg-base-200">
+          <li>
+            <h2 class="menu-title">Recently Updated</h2>
+            <ul>
+              <li><a>Item 1</a></li>
+              <li><a>Item 2</a></li>
+              <li><a>Item 3</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
