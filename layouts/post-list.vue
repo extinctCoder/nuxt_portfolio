056bc93c0a3ac5ac6965d8b078df4500564b951b
fiery.snowflake <extinctCoder@outlook.com>
2024-02-09 18:08:44 +0000

    Feb 10, 2024, 12:08 AM

--- layouts/post-list.vue
+++ layouts/post-list.vue
@@ -1,23 +1,24 @@
 <template>
   <NuxtLayout name="default">
     <div class="flex gap-8">
-      <div class="flex grow flex-col gap-4 bg-gray-400">
+      <div class="flex grow flex-col gap-4">
         <slot />
       </div>
       <div class="w-72 flex-none">
-        <div class="sticky top-0">
-          <ul class="menu">
-            <li>
-              <h2 class="menu-title">Recently Updated</h2>
-              <ul>
-                <li><NuxtLink>Writing a New Post</NuxtLink></li>
-                <li><NuxtLink>Getting Started</NuxtLink></li>
-                <li><NuxtLink>Text and Typography</NuxtLink></li>
-                <li><NuxtLink>Customize the Favicon</NuxtLink></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+        hello
+        <!-- <div class="sticky top-0"> -->
+        <!-- <ul class="menu">
+          <li>
+            <h2 class="menu-title">Recently Updated</h2>
+            <ul>
+              <li><NuxtLink>Writing a New Post</NuxtLink></li>
+              <li><NuxtLink>Getting Started</NuxtLink></li>
+              <li><NuxtLink>Text and Typography</NuxtLink></li>
+              <li><NuxtLink>Customize the Favicon</NuxtLink></li>
+            </ul>
+          </li>
+        </ul> -->
+        <!-- </div> -->
       </div>
     </div>
   </NuxtLayout>
