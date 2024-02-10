e2b6c010646f96c60c49f00c4eb3a0366b6ef6c7
fiery.snowflake <extinctCoder@outlook.com>
2024-02-10 18:41:13 +0000

    Feb 11, 2024, 12:41 AM

--- app.vue
+++ app.vue
@@ -4,6 +4,15 @@
   </NuxtLayout>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+useHead({
+  title: "Portfolio App",
+  meta: [{ name: "description", content: "My amazing site." }],
+  bodyAttrs: {
+    class: "test",
+  },
+  script: [{ innerHTML: "console.log('Hello world')" }],
+});
+</script>
 
 <style></style>
