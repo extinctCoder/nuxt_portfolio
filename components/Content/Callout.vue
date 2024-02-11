1c620118b52394fc6c8f62ce33b8adaacc865f52
fiery.snowflake <extinctCoder@outlook.com>
2024-02-11 18:46:47 +0000

    Feb 12, 2024, 12:46 AM

---
+++ components/Content/Callout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div
+      class="rounded-b border-t-4 border-teal-500 bg-teal-100 px-4 py-3 text-teal-900 shadow-md"
+      role="alert"
+    >
+      <div class="flex">
+        <div class="py-1">
+          <svg
+            class="mr-4 h-6 w-6 fill-current text-teal-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"
+            />
+          </svg>
+        </div>
+        <div>
+          <p class="font-bold">{{ props.title }}</p>
+          <p class="text-sm">
+            <slot />
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Callout",
+  },
+});
+</script>
+
+<style></style>
