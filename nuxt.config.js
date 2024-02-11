a936e8fd8e709324fb33dc0a067b41a81214ff3a
fiery.snowflake <extinctCoder@outlook.com>
2024-02-11 18:11:53 +0000

    Feb 12, 2024, 12:11 AM

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "nuxt-icon",
   ],
+  content: {},
   // image: {
   //   // Options
   // },
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,7 @@
+<template>
+  <div>this is catch all</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
