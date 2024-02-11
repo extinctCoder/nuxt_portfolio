81bd9497ee1e4da22c29449c5e5dfa7eaf542e1c
fiery.snowflake <extinctCoder@outlook.com>
2024-02-11 17:49:44 +0000

    Feb 11, 2024, 11:49 PM

--- components/sidebar/SidebarMenu.vue
+++ components/sidebar/SidebarMenu.vue
@@ -1,57 +1,10 @@
 <template>
   <ul class="menu menu-lg">
-    <!-- {{
-      sideMenu
-    }} -->
-    <!-- <a
-      v-for="item in navigation"
-      :key="item.name"
-      :href="item.href"
-      :class="[
-        item.current
-          ? 'bg-gray-900 text-white'
-          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-        'rounded-md px-3 py-2 text-sm font-medium',
-      ]"
-      :aria-current="item.current ? 'page' : undefined"
-      >{{ item.name }}</a
-    > -->
     <li v-for="nav_item in sideMenu" :key="nav_item.to">
       <NuxtLink :to="nav_item.to">
-        <Icon name="{{nav_item.title  }}" />
-        home
+        <Icon :name="nav_item.icon" />
+        {{ nav_item.title }}
       </NuxtLink>
-    </li>
-
-    <li>
-      <NuxtLink to="/">
-        <Icon name="heroicons:home-solid" />
-        home
-      </NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/blog">
-        <Icon name="heroicons:list-bullet-solid" />
-        blog
-      </NuxtLink>
-    </li>
-    <li>
-      <a>
-        <Icon name="heroicons:tag-solid" />
-        tags
-      </a>
-    </li>
-    <li>
-      <a>
-        <Icon name="heroicons:archive-box-solid" />
-        archives
-      </a>
-    </li>
-    <li>
-      <a>
-        <Icon name="heroicons:information-circle-solid" />
-        about
-      </a>
     </li>
   </ul>
 </template>
