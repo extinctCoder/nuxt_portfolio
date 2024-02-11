e1341e2494aed2c44910f3a13c0a4d65db30c5df
fiery.snowflake <extinctCoder@outlook.com>
2024-02-11 19:42:45 +0000

    Feb 12, 2024, 1:42 AM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -33,6 +33,16 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+export interface Props {
+  msg?: string;
+  labels?: string[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  msg: "hello",
+  labels: () => ["one", "two"],
+});
+</script>
 
 <style></style>
