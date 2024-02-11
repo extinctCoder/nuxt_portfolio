389f7b646a6ef1e3878a52c0222a9a347f76e236
fiery.snowflake <extinctCoder@outlook.com>
2024-02-11 19:36:09 +0000

    Feb 12, 2024, 1:36 AM

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,17 @@
 <template>
+  <div v-for="post in posts" :key="post._path">
+    {{ post.title }}
+  </div>
   <Card />
+  {{ posts }}
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: "post-list",
 });
-const { data: posts } = await useAsyncData("posts", () =>
+const { path } = useRoute();
+const { data: posts } = await useAsyncData(path, () =>
   queryContent("/blog").find(),
 );
 </script>
