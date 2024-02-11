2e98d7bed2bb1d0d2b77a1825943003e9a293ccd
fiery.snowflake <extinctCoder@outlook.com>
2024-02-11 18:03:10 +0000

    Feb 12, 2024, 12:03 AM

--- components/sidebar/SidebarFooter.vue
+++ components/sidebar/SidebarFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="menu menu-horizontal menu-lg items-center justify-around">
-    <li v-for="nav_item in navigation" :key="nav_item.href">
+    <li v-for="nav_item in navigation" :key="nav_item.title">
       <a :href="nav_item.href">
         <Icon :name="nav_item.icon" />
       </a>
@@ -10,13 +10,13 @@
 
 <script lang="ts" setup>
 const navigation = [
-  { title: "Fiverr", href: "/", icon: "jam:fiverr" },
   { title: "Linkedin", href: "/blog", icon: "jam:linkedin" },
   { title: "GitHub", href: "/projects", icon: "jam:github" },
+  { title: "Fiverr", href: "/", icon: "jam:fiverr" },
   { title: "Gitlab", href: "/uses", icon: "jam:gitlab" },
   {
-    title: "Whatsapp",
-    to: "/uses",
+    title: "WA",
+    href: "/uses",
     icon: "jam:whatsapp",
   },
 ];
