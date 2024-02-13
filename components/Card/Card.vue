04fff6097065173ea0ff3d5bf36cf601e8b35662
darthVader <extinctCoder@outlook.com>
2024-02-13 05:42:54 +0000

    Feb 13, 2024, 11:42 AM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -58,6 +58,22 @@
       </div>
     </div>
   </div>
+
+  <div class="card card-side bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+        alt="Movie"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">New movie is released!</h2>
+      <p>Click the button to watch on Jetflix app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Watch</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
