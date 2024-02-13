985b097eb1e928770393969ead2968e2284146db
fiery.snowflake <extinctCoder@outlook.com>
2024-02-13 19:33:54 +0000

    Feb 13, 2024, 7:33 PM

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,6 +2,10 @@
   <div class="prose mx-auto max-w-none">
     <ContentRenderer :value="post"></ContentRenderer>
   </div>
+  <NuxtLink v-if="prev" :to="prev._path">{{ prev.title }}</NuxtLink>
+  <NuxtLink v-if="next" :to="next._path">{{ next.title }}</NuxtLink>
+  {{ prev }}
+  hi
 </template>
 
 <script setup>
@@ -12,6 +16,7 @@ const { path } = useRoute();
 const { data: post } = await useAsyncData(path, () =>
   queryContent().where({ _path: path }).findOne(),
 );
+const { prev, next } = useContent();
 </script>
 
 <style></style>
