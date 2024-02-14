27b2819ccb27c1e64b3f7727a759b329e84a015a
darthVader <extinctCoder@outlook.com>
2024-02-14 09:16:09 +0000

    Feb 14, 2024, 3:16 PM

--- components/Card/Card.vue
+++ components/Card/Card.vue
@@ -48,7 +48,8 @@ const props = withDefaults(defineProps<CardInterface>(), {
   heading: "Lorem ipsum",
   to: "/",
   publish_date: undefined,
-  tags: undefined,
+  // tags: undefined,
+  tags: () => ["Lorem", "ipsum", "dolor", "sit"],
   image: undefined,
   body_text:
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio enim optio sunt. Ipsa et blanditiis rerum adipisci. Optio eligendi porro autem voluptates, ducimus dolores minus voluptatem omnis culpa mollitia eius!",
@@ -56,7 +57,9 @@ const props = withDefaults(defineProps<CardInterface>(), {
 
 const formattedTags = computed(() => {
   if (props.tags) {
-    return props.tags.map((tag) => tag.toLocaleUpperCase()).join(", ");
+    return props.tags
+      .map((tag) => tag.charAt(0).toUpperCase() + tag.slice(1))
+      .join(", ");
   }
   return "";
 });
