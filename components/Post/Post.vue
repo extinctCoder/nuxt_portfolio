38adf45f6482884de16406a2d89839c794f364f1
fiery.snowflake <extinctCoder@outlook.com>
2024-02-14 17:50:59 +0000

    Feb 14, 2024, 11:50 PM

--- components/Post/Post.vue
+++ components/Post/Post.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="flex gap-4">
-    <div class="prose w-full max-w-none prose-p:text-justify">
-      <slot name="post-body" />
+    <div class="flex flex-col gap-4">
+      <div class="h-4 bg-red-400">
+        <div
+          class="aspect-21/9"
+          style="
+            background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);
+          "
+        >
+          <div class="hero-overlay bg-opacity-60"></div>
+          <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+              <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+              <p class="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="prose w-full max-w-none prose-p:text-justify">
+        <slot name="post-body" />
+      </div>
     </div>
 
     <div class="w-10/12 min-w-64 max-w-96">
