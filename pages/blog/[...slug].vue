47c17471e5025f0e247d6a5e97852b6bccc6c0a6
darthVader <extinctCoder@outlook.com>
2024-02-14 06:47:38 +0000

    Feb 14, 2024, 12:47 PM

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,19 +1,19 @@
 <template>
   <!-- <template> -->
   <!-- <div class="prose mx-auto max-w-none"> -->
-  <div class="prose max-w-none">
+  <!-- <div class="prose max-w-none">
     <ContentRenderer :value="post"></ContentRenderer>
   </div>
   <TableOfContent :children="post.body.toc.links" />
-  {{ post.body.toc }}
+  {{ post.body.toc }} -->
   <!-- </template> -->
   <!-- <template #toc>toc</template> -->
 </template>
 
 <script setup>
-definePageMeta({
-  layout: "post-single",
-});
+// definePageMeta({
+//   layout: "post-single",
+// });
 const { path } = useRoute();
 const { data: post } = await useAsyncData(path, () =>
   queryContent().where({ _path: path }).findOne(),
