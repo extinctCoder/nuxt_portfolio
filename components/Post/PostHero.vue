ef058ff67e5d6018da09af2170d3ca90d25c372a
fiery.snowflake <extinctCoder@outlook.com>
2024-02-14 18:28:22 +0000

    Feb 15, 2024, 12:28 AM

--- components/Post/PostHero.vue
+++ components/Post/PostHero.vue
@@ -5,9 +5,13 @@
       src="/images/blog/mountains_2.jpg"
     />
     <div class="absolute bottom-0 flex w-full flex-row justify-between p-4">
-      <div class="prose">
-        <p>Your text here</p>
-        <p>Your text here</p>
+      <div class="avatar">
+        <div class="w-16 rounded">
+          <img
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            alt="Tailwind-CSS-Avatar-component"
+          />
+        </div>
       </div>
       <div class="prose">
         <p>Your text here</p>
