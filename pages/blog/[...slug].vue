cc90c48e305879a411cc1952aea6dc60501087fc
darthVader <extinctCoder@outlook.com>
2024-02-14 08:30:15 +0000

    Feb 14, 2024, 2:30 PM

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -5,26 +5,18 @@
     </template>
     <template #toc><TableOfContent :children="post.body.toc.links" /></template>
   </Post>
-
-  <!-- <template> -->
-  <!-- <div class="prose mx-auto max-w-none"> -->
-  <!-- <div class="prose max-w-none">
- 
-  </div>
-  <TableOfContent :children="post.body.toc.links" />
-  {{ post.body.toc }} -->
-  <!-- </template> -->
-  <!-- <template #toc>toc</template> -->
+  {{ breadcrumbs }}
 </template>
 
 <script setup>
-// definePageMeta({
-//   layout: "post-single",
-// });
 const { path } = useRoute();
 const { data: post } = await useAsyncData(path, () =>
   queryContent().where({ _path: path }).findOne(),
 );
+const breadcrumbs = route.value.matched.map((routeItem) => ({
+  text: routeItem.name,
+  to: routeItem.path,
+}));
 </script>
 
 <style></style>
