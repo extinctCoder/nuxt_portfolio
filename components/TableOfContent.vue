9459fb2ce16a9be1fa8aab594609bbc219a771f8
darthVader <extinctCoder@outlook.com>
2024-02-14 07:08:34 +0000

    Feb 14, 2024, 1:08 PM

--- components/TableOfContent.vue
+++ components/TableOfContent.vue
@@ -1,13 +1,16 @@
 <template>
   <ul class="menu">
-    <h2 class="menu-title">Contents</h2>
-    <li v-for="link in children" :key="link.id">
-      <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
-      <ul v-if="link.children">
-        <li v-for="child_link in link.children" :key="child_link.id">
-          <NuxtLink :to="`#${child_link.id}`">{{ child_link.text }}</NuxtLink>
-        </li>
-      </ul>
+    <li>
+
+      <h2 class="menu-title">Contents</h2>
+      <li v-for="link in children" :key="link.id">
+        <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+        <ul v-if="link.children">
+          <li v-for="child_link in link.children" :key="child_link.id">
+            <NuxtLink :to="`#${child_link.id}`">{{ child_link.text }}</NuxtLink>
+          </li>
+        </ul>
+      </li>
     </li>
   </ul>
 </template>
