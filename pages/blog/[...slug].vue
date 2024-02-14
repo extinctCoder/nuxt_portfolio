0c78409817f1b0c4bde84d3ceaf60ac58862565d
darthVader <extinctCoder@outlook.com>
2024-02-14 04:46:54 +0000

    Feb 14, 2024, 10:46 AM

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -28,6 +28,24 @@
       </div>
     </div>
   </div>
+  <ul class="menu w-56 rounded-box bg-base-200">
+    <li><a>Item 1</a></li>
+    <li>
+      <a>Parent</a>
+      <ul>
+        <li><a>Submenu 1</a></li>
+        <li><a>Submenu 2</a></li>
+        <li>
+          <a>Parent</a>
+          <ul>
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li><a>Item 3</a></li>
+  </ul>
 </template>
 
 <script setup>
