7dbfb8ba95fa5a5900c7367e2c3454d2dc97c6af
darthVader <extinctCoder@outlook.com>
2024-02-14 10:19:52 +0000

    Feb 14, 2024, 4:19 PM

--- components/Post/Post.vue
+++ components/Post/Post.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="flex gap-8 bg-red-400">
     <div class="prose w-full bg-amber-300">
-      <slot name="post-body" />
+      <slot name="post-body " />
     </div>
   </div>
+  <!-- <div class="hidden flex-1 bg-zinc-400 lg:block">
+       <div class="sticky top-0"> 
+
+      <ul class="menu">
+        <li>
+          <h2 class="menu-title">Recently Updated</h2>
+          <ul>
+            <li><NuxtLink>Writing a New Post</NuxtLink></li>
+            <li><NuxtLink>Getting Started</NuxtLink></li>
+            <li><NuxtLink>Text and Typography</NuxtLink></li>
+            <li><NuxtLink>Customize the Favicon</NuxtLink></li>
+          </ul>
+        </li>
+      </ul>
+      <slot name="toc" />
+      </div> 
+    </div> -->
 </template>
 
 <script lang="ts" setup></script>
