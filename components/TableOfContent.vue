afceca04ed61b9e5094ade5f9af8345715121dcc
darthVader <extinctCoder@outlook.com>
2024-02-14 06:04:52 +0000

    Feb 14, 2024, 12:04 PM

--- components/TableOfContent.vue
+++ components/TableOfContent.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    
-  </div>
+  <ul class="menu">
+    <li v-for="link in children" :key="link.id">
+      <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
+      <ul v-if="link.children">
+        <li v-for="child_link in link.children" :key="child_link.id">
+          <NuxtLink :to="`#${child_link.id}`">{{ child_link.text }}</NuxtLink>
+        </li>
+      </ul>
+    </li>
+  </ul>
+  {{ props }}
 </template>
 
 <script lang="ts" setup>
-
+// interface ToCInterface {
+//   links?: object;
+// }
+interface ToCInterface {
+  id?: string;
+  depth?: number;
+  text?: string;
+  children?: ToCInterface[];
+}
+const props = withDefaults(defineProps<ToCInterface>(), {});
 </script>
 
-<style>
-
-</style>
+<style></style>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,23 +1,9 @@
 <template>
   <div class="prose mx-auto max-w-none">
     <ContentRenderer :value="post"></ContentRenderer>
-    {{ post.body.toc }}
-    <br />
-    <br />
-    <br />
-    title: {{ post.body.toc.title }}
-    <br />
   </div>
-  <ul class="menu bg-base-200">
-    <li v-for="link in post.body.toc.links" :key="link.id">
-      <NuxtLink :to="`#${link.id}`">{{ link.text }}</NuxtLink>
-      <ul v-if="link.children">
-        <li v-for="child_link in link.children" :key="child_link.id">
-          <NuxtLink :to="`#${child_link.id}`">{{ child_link.text }}</NuxtLink>
-        </li>
-      </ul>
-    </li>
-  </ul>
+  <TableOfContent :links="post.body.toc.links" />
+  <!-- {{ post.body.toc.links }} -->
 </template>
 
 <script setup>
