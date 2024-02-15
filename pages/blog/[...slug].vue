480ef531f82cbede65e84c48899cdf3e96f18272
fiery.snowflake <extinctCoder@outlook.com>
2024-02-15 10:37:43 +0000

    Feb 15, 2024, 4:37 PM

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,28 +2,28 @@
   <Post>
     <template #post-hero>
       <PostHero
-        :title="post.title"
-        :update_date="post.updated_on"
-        :publish_date="post.published_at"
-        :avatar="post.avatar"
-        :cover="post.cover"
-        :cover_description="post.cover_description"
-        :author="post.author"
-        :source="post.source"
-        :read_time="post.read_time"
+        :title="post?.title"
+        :update_date="post?.updated_on"
+        :publish_date="post?.published_at"
+        :avatar="post?.avatar"
+        :cover="post?.cover"
+        :cover_description="post?.cover_description"
+        :author="post?.author"
+        :source="post?.source"
+        :read_time="post?.read_time"
       />
     </template>
     <template #post-body>
-      <ContentRenderer :value="post"></ContentRenderer>
+      <ContentRenderer v-if="post" :value="post"></ContentRenderer>
     </template>
     <template #toc>
-      <PostTableOfContent :children="post.body.toc.links" />
+      <PostTableOfContent :children="post?.body?.toc?.links" />
     </template>
   </Post>
   {{ path }}
 </template>
 
-<script setup>
+<script lang="ts" setup>
 const { path } = useRoute();
 const { data: post } = await useAsyncData(path, () =>
   queryContent().where({ _path: path }).findOne(),
