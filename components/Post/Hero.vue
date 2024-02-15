f176849f3f78363362db49204d16408402173d67
fiery.snowflake <extinctCoder@outlook.com>
2024-02-15 09:20:44 +0000

    Feb 15, 2024, 3:20 PM

--- components/Post/Hero.vue
+++ components/Post/Hero.vue
@@ -5,10 +5,11 @@
         :src="`/images/blog/${cover}`"
         class="aspect-21/9 max-h-80 w-full rounded object-cover object-center opacity-45 blur"
       />
-      <div
-        class="absolute top-0 flex w-full flex-row items-center justify-end gap-4 p-6"
-      >
+      <div class="absolute top-0 flex w-full flex-row items-center gap-4 p-6">
         hello
+      </div>
+      <div class="absolute right-0 top-0 bg-black bg-opacity-50 p-4 text-white">
+        <p>Your text here</p>
       </div>
       <div
         class="absolute bottom-0 flex w-full flex-row items-center gap-4 p-4"
