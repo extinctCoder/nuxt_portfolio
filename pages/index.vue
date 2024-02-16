ac8b35d277d6e1db431abe9dd38c154bd1092b6b
fiery.snowflake <extinctCoder@outlook.com>
2024-02-16 11:01:47 +0000

    Feb 16, 2024, 5:01 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  {{ pokemon }}
+  <!-- {{ pokemon }} -->
 </template>
 
 <script lang="ts" setup>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,33 @@ export default {
     extend: {
       // primary: "#FF6363",
 
+      colors: {
+        brand: {
+          linkedin: "#0077B5",
+          whatsapp: "#075E54",
+          fiverr: "#00B22D",
+          github: "#333333",
+          gitlab: "#FC6D26",
+          google: "#4285f4",
+          facebook: "#3B5998",
+          messenger: "#0084FF",
+          discord: "#5865F2",
+          instagram: "#E1306C",
+          reddit: "#FF4500",
+          telegram: "#0088CC",
+          twitter: "#1DA1F2",
+          ubuntu: "#DD4814",
+          wikipedia: "#000000",
+          youtube: "#FF0000",
+          android: "#A4C639",
+        },
+      },
+      fontFamily: {
+        dancing_script: "Dancing Script",
+        lato: "Lato",
+        roboto: "Roboto",
+      },
+
       aspectRatio: {
         "4/3": "4 / 3",
         "16/9": "16 / 9",
