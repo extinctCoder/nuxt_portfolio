1463efbaadbff73a380a97aa3cbe0a5d2d889156
darthVader <extinctCoder@outlook.com>
2024-02-20 16:01:00 +0600

    new update

---
+++ pages/test.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="btn" @click="open = true">btn</button>
+  <Dialog as="div" class="relative z-10" @close="open = false" :open="open">
+    <div class="fixed inset-0 bg-base-100 bg-opacity-80 transition-opacity" />
+
+    <div class="fixed inset-0 overflow-hidden">
+      <div class="absolute inset-0 overflow-hidden">
+        <div class="pointer-events-none fixed inset-y-0 flex max-w-full">
+          <DialogPanel class="pointer-events-auto relative w-screen max-w-md">
+            <div class="absolute left-72 top-8">
+              <button @click="open = false" class="btn btn-error">
+                <Icon name="heroicons:x-mark-solid" />
+              </button>
+            </div>
+
+            <div
+              class="md:w-12/12 flex h-full w-10/12 min-w-64 bg-base-200 md:max-w-96 lg:w-3/12"
+            >
+              <SidebarCard />
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </div>
+  </Dialog>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { XMarkIcon } from "@heroicons/vue/24/outline";
+
+const open = ref(true);
+</script>
